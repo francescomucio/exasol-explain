3fc6beae605f3469390ff2ef61fb88b6e0feb7fb
mucio <mucio@mucio.net>
2016-12-20 17:36:51 +0000

    Update profiling.lua

--- scripts/profiling.lua
+++ scripts/profiling.lua
@@ -1,6 +1,5 @@
-OPEN SCHEMA MICRO;
 CREATE OR REPLACE LUA SCRIPT "PROFILING" () RETURNS ROWCOUNT AS
-import("MICRO.SQLPROFILE", 'sqlprofile')
+import("SQLPROFILE", 'sqlprofile')
 
 sqlparsing.setsqltext(
         sqlprofile.explain(sqlparsing.getsqltext()))
